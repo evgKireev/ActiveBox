9c47e0b21dfa3e9343462217eb7b5e65e360b26e
evgKireev <bob1_k@mail.ru>
2022-06-10 14:41:40 +0300

    corrected btn menu

--- app/js/main.js
+++ app/js/main.js
@@ -9,7 +9,11 @@ $(function() {
   $('.menu__btn').on('click', function() {
     $('.menu__list').toggleClass('menu__list--active');
     $('.menu__btn').toggleClass('menu__btn--active');
+  });
 
+  $('.menu__list-item').on('click', function() {
+    $('.menu__list').removeClass('menu__list--active');
+    $('.menu__btn').removeClass('menu__btn--active');
   });
 
 });
